0b091a0e9b351bd33edc87afcf8b6c919dbcece2
yaroslavx <yakupov.yaroslav@icloud.com>
2025-09-27 08:53:02 +0300

    MAINTAIN: Refactored

--- Catalog.Service/Program.cs
+++ Catalog.Service/Program.cs
@@ -8,9 +8,6 @@ using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
-BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
 builder.Services.AddControllers(options =>
 {
     options.SuppressAsyncSuffixInActionNames = false;
@@ -20,20 +17,9 @@ builder.Services.AddCors();
 
 builder.Services.AddOpenApi();
 
-var serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+builder.Services.AddMongo()
+    .AddMongoRepository<Item>("items");
 
-builder.Services.AddSingleton(serviceProvider =>
-{
-    var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-    var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-    return mongoClient.GetDatabase(serviceSettings.ServiceName);
-});
-
-builder.Services.AddSingleton<IRepository<Item>>(serviceProvider =>
-{
-    var database = serviceProvider.GetService<IMongoDatabase>();
-    return new MongoRepository<Item>(database, "items");
-});
 
 var app = builder.Build();
 
