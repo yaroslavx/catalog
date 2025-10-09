c89b9b3d7c1903536005033f1fddea35725381ad
yaroslavx <yakupov.yaroslav@icloud.com>
2025-10-09 20:58:25 +0300

    MAINTAIN: Created Contracts project

---
+++ Contracts/Contracts.cs
@@ -0,0 +1,7 @@
+namespace Contracts;
+
+public record CatalogItemCreated(Guid ItemId, string Name, string Description);
+
+public record CatalogItemUpdated(Guid ItemId, string Name, string Description);
+
+public record CatalogItemDeleted(Guid ItemId);
