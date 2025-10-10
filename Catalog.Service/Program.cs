9c124fd6d0c09fc271f3ffe2cf33741adbab2ebf
yaroslavx <yakupov.yaroslav@icloud.com>
2025-10-10 21:57:37 +0300

    REFACTOR

--- Catalog.Service/Program.cs
+++ Catalog.Service/Program.cs
@@ -1,23 +1,8 @@
 using Catalog.Service.Entities;
-using Catalog.Service.Settings;
+using Common.MassTransit;
 using Common.MongoDB;
-using Common.Settings;
-using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddMassTransit(x =>
-{
-    x.UsingRabbitMq((context, configurator) =>
-    {
-        var rabbitMQSettings = builder.Configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
-        configurator.Host(rabbitMQSettings.Host);
-        configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(
-            builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>().ServiceName, false));
-    });
-});
-
-builder.Services.AddMassTransitHostedService();
 
 builder.Services.AddControllers(options =>
 {
@@ -29,7 +14,8 @@ builder.Services.AddCors();
 builder.Services.AddOpenApi();
 
 builder.Services.AddMongo()
-    .AddMongoRepository<Item>("items");
+    .AddMongoRepository<Item>("items")
+    .AddMassTransitWithRabbitMQ();
 
 
 var app = builder.Build();
--- Catalog.Service/Settings/RabbitMQSettings.cs
+++
@@ -1,6 +0,0 @@
-namespace Catalog.Service.Settings;
-
-public class RabbitMQSettings
-{
-    public string Host { get; init; }
-}
